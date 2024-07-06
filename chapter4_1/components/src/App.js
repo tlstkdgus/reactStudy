e2d0a84507a58f4781f071a101f6f06508164068
tlstkdgus <tlstkdgus9@naver.com>
2024-07-06 14:37:59 +0900

    chapter4_1

--- chapter4_1/components/src/App.js
+++ chapter4_1/components/src/App.js
@@ -1,6 +1,16 @@
-function app(){
+import './App.css';
+
+function App(){
+  // 여기는 js할 때와 똑같이 주석을 입력합니다! 왜냐면 여기는 JS의 영역이니깐요 :)
   return(
-    <h1>안녕, 라이캣!</h1>
+
+    /** 여기서부터는 jsx! 주석은 이런식으로 작성합니다. 한줄 주석은 따로 존재하지 않습니다. */
+    <div>
+      <h1>안녕, 라이캣 1호!</h1>
+      <h1>안녕, 라이캣 2호!</h1>
+      <div className = 'newClass'/>
+    </div>
+    
   )
 }
 export default App;
