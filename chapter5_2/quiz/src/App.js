76701b33d313422fed25dfbd79174e06c56a5013
tlstkdgus <tlstkdgus9@naver.com>
2024-07-08 00:16:19 +0900

    chapter5

---
+++ chapter5_2/quiz/src/App.js
@@ -0,0 +1,19 @@
+import { DisplayMood } from "./components/DisplayMood/DisplayMood";
+import { MenuList } from "./components/MenuList/MenuList";
+import './App.css';
+import { useState } from "react";
+function App() {
+
+  const [currentMood, setCurrentMood] = useState('');
+
+  return (
+    <>
+      <h1>오늘의 기분을 선택해 주세요!</h1>
+      <div className="app-main">
+        <MenuList mood = {currentMood} onItemClick = {setCurrentMood}/>
+        <DisplayMood mood = {currentMood}/>
+      </div>
+    </>
+  );
+}
+export default App;
