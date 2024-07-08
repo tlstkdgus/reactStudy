4df25c75429024865b8a89692d6ff774440096a4
tlstkdgus <tlstkdgus9@naver.com>
2024-07-08 15:31:41 +0900

    chapter6

--- chapter6_2/event/src/App.js
+++ chapter6_2/event/src/App.js
@@ -1,8 +1,23 @@
+import {useState} from 'react';
+import { HomePage } from './components/HomePage';
+import { Login } from './components/Login';
+
 function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+
+  const user = {
+    idUser: 'jaehyun@weniv.com',
+    pwUser: '1234'
+  }
+
+  const [login, setLogin] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+
+  return(
+    <>
+      {login ? <HomePage/> : <Login infoUser={user} setLogin={setLogin}/>}
+      {modalShow && <Modal/>}
+    </>
+  )
+  
 }
 export default App;
